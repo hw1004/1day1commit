b582c219b690f7b7d1d6eec8aefe76b5917f3830
[Hyewon <henajung10@gmail.com>
2024-01-21 10:39:24 +0900

    commit1

--- algorithms/Programmers/크기가 작은 부분 문자열/sol1.py
+++
@@ -1,9 +0,0 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/147355
-
-def solution(t, p):
-    part_string_length = len(p)
-    count_part = 0
-    for i in range(len(t) - part_string_length + 1):
-        if int(t[i:i+part_string_length]) <= int(p):
-            count_part += 1
-    return count_part
