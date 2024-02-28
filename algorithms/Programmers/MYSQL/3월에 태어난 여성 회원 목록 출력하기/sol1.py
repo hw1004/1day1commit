7885d9ce869a1450defd56e57437fa2ee8499d4c
[Hyewon <henajung10@gmail.com>
2024-02-28 21:04:05 +0900

    sql practice

---
+++ algorithms/Programmers/MYSQL/3월에 태어난 여성 회원 목록 출력하기/sol1.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/131120
+# 10 minutes
+
+# SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, "%Y-%m-%d") AS DATE_OF_BIRTH
+# FROM MEMBER_PROFILE
+# WHERE (TLNO IS NOT NULL) AND (MONTH(DATE_OF_BIRTH) = "3") AND (GENDER = 'W')
+# ORDER BY MEMBER_ID ASC;
