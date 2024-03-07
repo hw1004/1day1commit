82950d608dc4122433829a575f61aac9c41122bf
[Hyewon <henajung10@gmail.com>
2024-03-07 22:26:31 +0900

    sql practice

---
+++ algorithms/Programmers/MYSQL/12세이하인여자환자목록출력하기/sol1.py
@@ -0,0 +1,8 @@
+# # https://school.programmers.co.kr/learn/courses/30/lessons/132201
+
+# -- 코드를 입력하세요
+# SELECT PT_NAME, PT_NO, GEND_CD, AGE, CASE WHEN TLNO IS NULL THEN 'NONE' ELSE TLNO END
+# FROM PATIENT
+# WHERE AGE <= 12 
+# AND GEND_CD = 'W'
+# ORDER BY AGE DESC, PT_NAME ASC;
