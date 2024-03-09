592d4039a6d65559b47899c89121629aae57b24e
[Hyewon <henajung10@gmail.com>
2024-03-09 11:56:41 +0900

    sql

---
+++ algorithms/Programmers/MYSQL/최댓값 구하기/sol1.py
@@ -0,0 +1,6 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/59415
+
+# -- 코드를 입력하세요
+# SELECT DATETIME AS '시간'
+# FROM ANIMAL_INS
+# WHERE DATETIME = (SELECT MAX(DATETIME) FROM ANIMAL_INS);
